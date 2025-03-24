b401aa1d3afe0439f11dac9a54c6ece6cd4d9a48
TheMadTitan777 <bukunmiadesanya@gmail.com>
2025-03-24 22:35:53 +0100

    fix

--- server.js
+++ server.js
@@ -35,8 +35,6 @@ app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "public", "intro.html"));
 });
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-
 
 // ✅ Middleware
 app.use(cors({
